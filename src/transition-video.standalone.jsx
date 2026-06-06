e17023d7169dda0f75272c7d8589c4597746ca94
krati1611 <lodhakrati3@gmail.com>
2026-06-06 10:23:32 +0530

    prioritize network streaming for safari compatibility

--- src/transition-video.standalone.jsx
+++ src/transition-video.standalone.jsx
@@ -47,12 +47,12 @@ function LLVideoSection({
   const inputRef = React.useRef(null);
 
   const defaults = React.useMemo(() => ([
-    window.__resources?.[`uploads/${slug}.mp4`],
-    window.__resources?.[`uploads/${slug}.webm`],
-    window.__resources?.[`uploads/${slug}.mov`],
     `uploads/${slug}.mp4`,
     `uploads/${slug}.webm`,
     `uploads/${slug}.mov`,
+    window.__resources?.[`uploads/${slug}.mp4`],
+    window.__resources?.[`uploads/${slug}.webm`],
+    window.__resources?.[`uploads/${slug}.mov`],
   ].filter(Boolean)), [slug]);
 
   React.useEffect(() => {
